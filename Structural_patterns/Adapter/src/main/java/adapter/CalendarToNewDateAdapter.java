c54edbf535ea8b9e9eddc2bc3d2a36e59ef3df7d
Thanh0201 <thanhho@metropolia.fi>
2026-03-24 08:58:00 +0200

    Adapter - Adapter converts the interface of an existing class into another interface expected by the client. It acts as a wrapper that translates calls, allowing legacy or third party code to integrate smoothly with new systems.

---
+++ Structural_patterns/Adapter/src/main/java/adapter/CalendarToNewDateAdapter.java
@@ -0,0 +1,74 @@
+package main.java.adapter;
+
+import java.util.Calendar;
+
+public class CalendarToNewDateAdapter implements NewDateInterface{
+    private Calendar calendar;
+    public CalendarToNewDateAdapter() {
+        this.calendar = Calendar.getInstance();
+    }
+
+    @Override
+    public void setDay(int day) {
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+    }
+
+    @Override
+    public void setMonth(int month) {
+        calendar.set(Calendar.MONTH, month);
+    }
+
+    @Override
+    public void setYear(int year) {
+        calendar.set(Calendar.YEAR, year);
+    }
+
+    @Override
+    public int getDay() {
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    @Override
+    public int getMonth() {
+        return calendar.get(Calendar.MONTH);
+    }
+
+    @Override
+    public int getYear() {
+        return calendar.get(Calendar.YEAR);
+    }
+
+    @Override
+    public void advanceDays(int days) {
+        calendar.add(Calendar.DAY_OF_MONTH, days);
+    }
+
+    public String getNameDay() {
+        int n = calendar.get(Calendar.DAY_OF_WEEK);
+
+        switch (n) {
+            case Calendar.MONDAY:
+                return "Monday";
+            case Calendar.TUESDAY:
+                return "Tuesday";
+            case Calendar.WEDNESDAY:
+                return "Wednesday";
+            case Calendar.THURSDAY:
+                return "Thursday";
+            case Calendar.FRIDAY:
+                return "Friday";
+            case Calendar.SATURDAY:
+                return "Saturday";
+            case Calendar.SUNDAY:
+                return "Sunday";
+            default:
+                return "Unknown";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getNameDay() + "," + getDay() + "/" + getMonth() + "/" + getYear();
+    }
+
+}
---
+++ Structural_patterns/Adapter/src/main/java/adapter/Main.java
@@ -0,0 +1,23 @@
+package main.java.adapter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        NewDateInterface date = new CalendarToNewDateAdapter();
+
+        date.setDay(22);
+        date.setMonth(3);
+        date.setYear(2026);
+
+        System.out.println("Origin day: " + date);
+
+        date.advanceDays(-10);
+        System.out.println("The day 10 days before: " + date);
+
+        date.advanceDays(10);
+        System.out.println("The day 10 days later: " + date);
+
+        date.advanceDays(40);
+        System.out.println("The day 40 days later: " + date);
+    }
+}
